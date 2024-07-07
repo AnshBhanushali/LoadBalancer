a92e47cb194d8068370e0fe48361d064e6af3970
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-06 21:27:11 -0400

    created backend servers

--- load_balancer/load_balancer.py
+++ load_balancer/load_balancer.py
@@ -1,2 +1,12 @@
 from flask import Flask, request, jsonify
 import requests
+
+app = Flask(__name__)
+
+# These are the backend server the project would work on
+backended_servers = [
+    'http://localhost:5001',
+    'http://localhost:5002'
+]
+
+current_server = 0
