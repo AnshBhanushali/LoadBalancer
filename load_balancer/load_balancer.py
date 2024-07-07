b554875ac2b56d4ea48d8abb0fecb2f97a27ef4c
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-06 21:34:14 -0400

    handling requests

--- load_balancer/load_balancer.py
+++ load_balancer/load_balancer.py
@@ -4,9 +4,25 @@ import requests
 app = Flask(__name__)
 
 # These are the backend server the project would work on
-backended_servers = [
+backend_servers = [
     'http://localhost:5001',
     'http://localhost:5002'
 ]
 
 current_server = 0
+
+@app('/<path:path>', methods = ['GET', 'POST', 'PUT', 'DELETE'])
+def load_balancer(path):
+    global current_server
+    backend_url = f"{backend_servers[current_server]}/{path}"
+
+    # This pushes the incomming request to backend servers
+    if request.method == 'GET':
+        response = requests.get(backend_url, params=request.args)
+    elif request.method == 'POST':
+        response = requests.post(backend_url, json=request.json)
+    elif request.method == 'PUT':
+        response = requests.put(backend_url, json=request.json)
+    elif request.method == 'DELETE':
+        response = requests.delete(backend_url)
+        
