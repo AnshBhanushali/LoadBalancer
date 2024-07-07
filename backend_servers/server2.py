9b3b7b0e65123a4666afb430b84a1a37f023ac22
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-07 14:41:36 -0400

    created handle request functionality for server 2

--- backend_servers/server2.py
+++ backend_servers/server2.py
@@ -1,2 +1,13 @@
 from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route('<path:path>', methods = ['GET', 'POST', 'PUT', 'DELETE'])
+def handle_request(path):
+    return jsonify({
+        "message": "Response from server 2",
+        "path": path,
+        "method": request.method,
+        "args": request.args,
+        "json": request.json,
+    })
